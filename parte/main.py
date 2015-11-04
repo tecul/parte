c4247319fe272ad189c92485c84eece882d361f3
ekim <ekim.tecul@yahoo.fr>
2015-11-04 21:05:32 +0100

    use info from xml feed

--- parte/main.py
+++ parte/main.py
@@ -2,40 +2,34 @@ import sys
 import urllib2
 import json
 import re
+import xml.etree.ElementTree as ET
 
 from downloader import Downloader
 
-bitrate2index = {"800": "HTTP_MP4_MQ_1",
-                 "1500": "HTTP_MP4_EQ_1",
-                 "2200": "HTTP_MP4_SQ_1"}
+def _get_download_url(url):
+    ns = {'media': 'http://search.yahoo.com/mrss/'}
+    feed = urllib2.urlopen("http://www.arte.tv/papi/tvguide-flow/feeds/videos/fr.xml?type=ARTE_PLUS_SEVEN&player=false").read()
+    root = ET.fromstring(feed)
+    items = root.findall(".//item")
+    for item in items:
+        if item.find('link').text == url:
+            return item.find(".//media:content[@bitrate='800']", ns).attrib['url']
 
-def _get_download_url(url, bitrate="800"):
-    res = re.search('http://www.arte.tv/guide/fr/([\d-]+)/', url)
-    if res:
-        json_url = "http://arte.tv/papi/tvguide/videos/stream/player/F/%s_PLUS7-F/ALL/ALL.json" % res.group(1)
-        playlist = json.loads(urllib2.urlopen(json_url).read())
-        #print json.dumps(playlist['videoJsonPlayer']["VSR"][bitrate2index[bitrate]], sort_keys=True, indent=4, separators=(',', ': '))
-        return playlist['videoJsonPlayer']["VSR"][bitrate2index[bitrate]]['url']
-    res = re.search('vid=([\d-]+)', url)
-    if res:
-        json_url = "http://arte.tv/papi/tvguide/videos/stream/player/F/%s_PLUS7-F/ALL/ALL.json" % res.group(1)
-        playlist = json.loads(urllib2.urlopen(json_url).read())
-        #print json.dumps(playlist['videoJsonPlayer']["VSR"][bitrate2index[bitrate]], sort_keys=True, indent=4, separators=(',', ': '))
-        return playlist['videoJsonPlayer']["VSR"][bitrate2index[bitrate]]['url']
-
-    return ""
+    return None
 
 def get_all_prg():
-    raw = urllib2.urlopen("http://www.arte.tv/guide/fr/plus7.json?page=1&per_page=1000").read()
-    videos = json.loads(raw)['videos']
+    ns = {'media': 'http://search.yahoo.com/mrss/'}
+    feed = urllib2.urlopen("http://www.arte.tv/papi/tvguide-flow/feeds/videos/fr.xml?type=ARTE_PLUS_SEVEN&player=false").read()
+    root = ET.fromstring(feed)
+    items = root.findall(".//item")
     res = []
-    for video in videos:
-        res.append({'title': video['title'],
-                    'description': video['desc'],
-                    'url': video['url'],
-                    'poster_url': video['image_url'],
-                    'category': 'unknown',
-                    'duration': video['duration'] * 60,
+    for item in items:
+        res.append({'title': item.find('title').text,
+                    'description': item.find('description').text,
+                    'url': item.find('link').text,
+                    'poster_url': item.find(".//media:thumbnail", ns).attrib['url'],
+                    'category': item.find('category').text,
+                    'duration': int(item.find(".//media:content[@bitrate='800']", ns).attrib['duration']) * 60,
                     'channel': 'arte'})
 
     return json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '))
